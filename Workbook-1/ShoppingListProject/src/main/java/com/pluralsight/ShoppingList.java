ecf2f8b5899f7dcc0aecdab1b8b2e605b392798e
Moth-beacon <gomeztimothy234@gmail.com>
2025-04-10 09:32:27 -0400

    inital commit

---
+++ Workbook-1/ShoppingListProject/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Shopping List: ");
+        System.out.println("milk");
+        System.out.println("bread");
+        System.out.println("lobster");
+        System.out.println("salmon");
+        System.out.println("apples");
+        System.out.println("toothpaste");
+        System.out.println("ramen");
+        System.out.println("hot sauce");
+        System.out.println("cabbage");
+        System.out.println("leaks");
+
+    }
+}
---
+++ Workbook-1/first-maven-project/src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("First ever application in intelliJ!");
+        System.out.println("second line, just cause");
+    }
+}
